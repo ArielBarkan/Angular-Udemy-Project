5eefa103a536431a6183a721836455ce23be4cdf
ArielBarkan <arielbarkan@gmail.com>
2020-04-08 00:31:42 +0300

    2 files modified

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -5,9 +5,9 @@ import {Recipe} from '../shared/models/recipe.model';
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe("Schnitzel","A schnitzel is meat", "../assets/images/recipes/1.jpg"),
-    new Recipe("Fried egg","A fried egg is a cooked dish", "../assets/images/recipes/2.jpg"),
-    new Recipe("Mejadra ","Mejadra consists of cooked lentils together with groats", "../assets/images/recipes/3.jpg")
+    new Recipe("Schnitzel","A schnitzel is meat", "https://cdn.jsdelivr.net/gh/ArielBarkan/Recipe-list@master/src/assets/images/recipes/1.jpg"),
+    new Recipe("Fried egg","A fried egg is a cooked dish", "https://cdn.jsdelivr.net/gh/ArielBarkan/Recipe-list@master/src/assets/images/recipes/2.jpg"),
+    new Recipe("Mejadra ","Mejadra consists of cooked lentils together with groats", "https://cdn.jsdelivr.net/gh/ArielBarkan/Recipe-list@master/src/assets/images/recipes/3.jpg")
   ];
 
 
